32ba344e5a5ea42937c5704e25795c47d660cc39
김재형 <wogud2437@naver.com>
2024-11-30 14:31:43 +0900

    1

--- src/main/java/com/example/final_jj/kafka/config/KafkaConfig.java
+++ src/main/java/com/example/final_jj/kafka/config/KafkaConfig.java
@@ -7,6 +7,8 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
